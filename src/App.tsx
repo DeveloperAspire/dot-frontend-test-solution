c90230537465643c3be572de2a9a1571b67835f1
Franklin Okolie <franklinokolie86@gmail.com>
2023-03-21 22:32:40 +0100

    feat: worked on setting up the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import './App.css';
-import Ballot from './Components/Ballot/Ballot';
+import Ballot from "./components/Ballot/Ballot";
 
 function App() {
   // Feel free to remove the contents of the header tag to make more room for your code
   return (
     <div className="App">
       <header className="App-header">
-        <img src={'https://via.placeholder.com/150'} className="App-logo" alt="logo" />
+        <img
+          src={"https://via.placeholder.com/150"}
+          className="App-logo"
+          alt="logo"
+        />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
